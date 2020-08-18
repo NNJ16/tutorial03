8adbbf78a63a94d902fee42e049c07681e8864f6
NNJ16 <nethsera@gmail.com>
2020-08-18 23:43:00 +0530

    Initial Commit by NNJ16

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,14 +2,17 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
-    EditText num1,num2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,19 @@ public class FirstActivity extends AppCompatActivity {
     public void sendMessage(View view)
     {
         Intent intent =  new Intent(FirstActivity.this,SecondActivity.class);
-        num1 = findViewById(R.id.txt_n1);
-        num2 = findViewById(R.id.txt_n2);
-        intent.putExtra("number1",num1.toString());
-        intent.putExtra("number2",num2.toString());
+        EditText num1 = findViewById(R.id.txt_num1);
+        EditText num2 = findViewById(R.id.txt_num2);
+        intent.putExtra("number1",num1.getText().toString());
+        intent.putExtra("number2",num2.getText().toString());
         startActivity(intent);
 
+        Context context = getApplicationContext();
+        CharSequence message = "You just clicked the OK button";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context,message,duration);
+        toast.show();
+
+        toast.setGravity(Gravity.TOP|Gravity.LEFT,0,0);
     }
 }
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -2,13 +2,60 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    EditText txt_n1,txt_n2;
+    TextView lbl_ans;
+    int num1,num2,ans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent =  getIntent();
+        String n1 = intent.getStringExtra("number1");
+        String n2 = intent.getStringExtra("number2");
+
+        num1 = Integer.parseInt(n1);
+        num2 = Integer.parseInt(n2);
+
+        txt_n1 = findViewById(R.id.txt_n1);
+        txt_n2 = findViewById(R.id.txt_n2);
+        lbl_ans =findViewById(R.id.lbl_ans);
+
+        txt_n1.setText(n1);
+        txt_n2.setText(n2);
+
+    }
+
+    public void Add(View view)
+    {
+        ans = num1 + num2;
+        lbl_ans.setText(Integer.toString(ans));
+    }
+
+    public void Subtraction(View view)
+    {
+        ans = num1 - num2;
+        lbl_ans.setText(Integer.toString(ans));
+    }
+
+    public void Devision(View view)
+    {
+        double ans = (double) num1 / num2;
+        lbl_ans.setText(Double.toString(ans));
+    }
+
+    public void Multiplication(View view)
+    {
+        ans = num1 * num2;
+        lbl_ans.setText(Integer.toString(ans));
     }
 }
